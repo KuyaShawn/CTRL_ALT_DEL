57e98ae7ed672b2f08dbe90a06693a3a85be0d95
Madmanmayson <dylanhardy97@gmail.com>
2021-02-09 14:05:03 -0800

    Add initial admin home page.

---
+++ admin/home.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin Home Page</title>
+</head>
+<body>
+<div class="row">
+    <div class="col-2 bg-light">
+
+    </div>
+    <div class="col-8">
+        <div>
+            <h2>Pending Approval</h2>
+        </div>
+    </div>
+    <div class="col-2 bg-light">
+
+    </div>
+</div>
+</body>
+</html>
