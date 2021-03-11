c7aa5f7db2dafc217daef4ddc6ae11398524897f
Madmanmayson <dylanhardy97@gmail.com>
2021-03-11 00:24:41 -0800

    Add create endpoint for v1/company/create.php

---
+++ api/v1/company/create.php
@@ -0,0 +1,76 @@
+<?php
+// required headers
+header('Allow: POST'); //Required header for 405 error codes per RFC 7231 standards
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Content-Type: application/json; charset=UTF-8");
+
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    http_response_code(405);
+    echo json_encode(array('error-msg' => 'Please use a POST request to access this endpoint.'));
+    die();
+}
+
+//include_once '/home/ctrlaltd/PDOuserconnect.php';
+include_once "../core/PDOuserconnect.php";
+include_once "../objects/company.php";
+
+$database = new Database();
+$company = new Company($database->connect());
+
+//Data being sent a stringified json for now so we are streaming input to an array rather then superglobal
+//Might be changed depending on cURL configuarion.
+$data = json_decode(file_get_contents("php://input"));
+
+if( //Checking to make sure all
+    !empty($data->name) &&
+    !empty($data->url) &&
+    !empty($data->city) &&
+    !empty($data->state) &&
+    !empty($data->country) &&
+    !empty($data->about) &&
+    !empty($data->category) &&
+    !empty($data->email)
+){
+    $company->name = $data ->name;
+    $company->url = $data ->url;
+    $company->city = $data ->city;
+    $company->state = $data ->state;
+    $company->country = $data ->country;
+    $company->about = $data ->about;
+    $company->category = $data ->category;
+    $company->email = $data ->email;
+
+    if(!empty($data->tag_cloud)){
+        $company->tag_cloud = $data->tag_cloud;
+    }
+
+    if($company->create()){
+        http_response_code(201);
+        
+        echo json_encode(array('msg' => 'Company created successfully'));
+    } else {
+        http_response_code(503);
+        
+        echo json_encode(array('error-msg' => 'Failed to create company'));
+    }
+
+
+} else {
+    http_response_code(400);
+    echo json_encode(array('error-msg' => 'Not all required data fields were provided'));
+}
+
+/* Required Fields:
+Name *
+Url *
+Country *
+State *
+City *
+Service Area
+About *
+Category *
+
+private email *
+
--- api/v1/objects/company.php
+++ api/v1/objects/company.php
@@ -3,6 +3,9 @@ class Company{
 
     private $connection;
     private $tableName = "company";
+    private $insertColumns = [];
+
+    public $errorString;
 
     public $id;
     public $name;
@@ -48,5 +51,51 @@ class Company{
         $this->state = $result['state'];
         $this->tag_cloud = $result['tag_cloud'];
         $this->url = $result['url'];
+    }
+
+    public function create(){
+
+        //Sanitize inputs and get only the initialized ones, putting it in $includedColumns (ignores nulls)
+        $this->name = $this->sanitizeCreateInputs($this->name, "name");
+        $this->about = $this->sanitizeCreateInputs($this->about, "about");
+        $this->category = $this->sanitizeCreateInputs($this->category, "category");
+        $this->city = $this->sanitizeCreateInputs($this->city, "city");
+        $this->country = $this->sanitizeCreateInputs($this->country, "country");
+        $this->email = $this->sanitizeCreateInputs($this->email, "email");
+        $this->phone = $this->sanitizeCreateInputs($this->phone, "phone");
+        $this->state = $this->sanitizeCreateInputs($this->state, "state");
+        $this->tag_cloud = $this->sanitizeCreateInputs($this->tag_cloud, "tag_cloud");
+        $this->url = $this->sanitizeCreateInputs($this->url, "url");
+
+        $query = "INSERT INTO {$this->tableName} SET " . implode(', ', $this->insertColumns);
+
+        $statement = $this->connection->prepare($query);
+
+        //Required insert statements that should never be null.
+        $statement->bindParam(':name', $this->name);
+        $statement->bindParam(':about', $this->about);
+        $statement->bindParam(':category', $this->category);
+        $statement->bindParam(':city', $this->city);
+        $statement->bindParam(':country', $this->country);
+        $statement->bindParam(':email', $this->email);
+        $statement->bindParam(':state', $this->state);
+        $statement->bindParam(':url', $this->url);
+
+        //Optional Data fields that will be bound if exist
+        if(!empty($this->phone)){$statement->bindParam(':phone', $this->phone);}
+        if(!empty($this->tag_cloud)){$statement->bindParam(':tag_cloud', $this->tag_cloud);}
+
+        if($statement->execute()){
+            return true;
+        }
+        return false;
+    }
+
+    private function sanitizeCreateInputs($data, $name){
+        if($data != null){
+            $data = htmlspecialchars(strip_tags($data)); //prevents XSS
+            array_push($this->insertColumns, "$name=:$name");
+        }
+        return $data;
     }
 }
