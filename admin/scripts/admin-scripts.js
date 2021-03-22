1d6e5b3c7af0d94f22c264547db89df915435577
Madmanmayson <dylanhardy97@gmail.com>
2021-03-22 14:27:44 -0700

    Add new endpoint `admin/company/reject.php`

--- admin/scripts/admin-scripts.js
+++ admin/scripts/admin-scripts.js
@@ -22,7 +22,20 @@ function acceptCompany(companyId){
 }
 
 function rejectCompany(companyId){
+    fetch('https://api.ctrl-alt-delete.greenriverdev.com/v1/admin/company/reject.php', {
+        method: 'PATCH',
+        body: JSON.stringify({
+            id: companyId,
+            status: 'REJECTED',
+            reason:  'Not enough information'
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+        }
+    })
+        .then(response => response.json())
+        .then(json => alert(json.message));
+
     document.getElementById(companyId).style.display = 'none';
     document.getElementById(companyId).classList.remove('d-flex');
-    alert("All Power Labs successfully rejected from catalog");
 }
